c7dc548e37e7a80eaadb36522854d968d858c093
anamariaagudelo <anamariaagudeloisi@gmail.com>
2019-03-20 16:40:04 -0500

    Dia 1 y Dia 2 OK

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Proptypes from 'prop-types';
+import Dashboard from './components/pages/dashboardPage';
+import DetailPage from './components/pages/detailPage';
+import "semantic-ui-css/semantic.min.css"
+
+
+const App = ({location}) => (
+  <div className="ui container">
+    <Switch>
+      <Route location = {location} path="/detail" exact component = {DetailPage}/>
+      <Route location = {location} path="/" exact component = {Dashboard}/>
+    </Switch>
+  </div>
+);
+
+
+App.prototype = {
+  location : Proptypes.shape({
+    pathname: Proptypes.string.isRequired
+  }).isRequired
+}
+
+export default App;
---
+++ src/components/pages/detailPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class Detail extends React.Component{
+    state ={}
+
+    render(){
+        return(
+            <div>
+                <h1>Detail</h1>
+                <Link to="/">go back to Dasboard</Link>
+            </div>
+        );
+    }
+}
+
+
+export default Detail;
