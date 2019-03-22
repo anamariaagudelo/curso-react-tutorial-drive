e3b70583568ae2201e69f1884d72eab68813d4fa
anamariaagudelo <anamariaagudeloisi@gmail.com>
2019-03-22 11:07:58 -0500

    Tarea Dia 4- Punto 1-Editar el componente  src/components/pages/statePage.js para que trabaje con redux. OK

--- src/components/pages/statePage.js
+++ src/components/pages/statePage.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Button, Segment } from 'semantic-ui-react'
+import { increaseLikes } from '../../actions/character'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+
 
 
 class stateLocal extends React.Component {
     state = { likes: 0 }
-
-    addLike = () => {
-        this.setState({
-            likes: this.state.likes + 1
-        });
-    }
 
     render() {
         const { likes } = this.state
@@ -17,16 +15,22 @@ class stateLocal extends React.Component {
         return (
             <Segment>
                 <h1>Local state</h1>
-            <Button onClick= {this.addLike}
-                color='red'
-                content='Like'
-                icon='heart'
-                label={{basic:true, color:'red', pointing:'left',content: likes}}
+                <Button onClick={this.props.increaseLikes}
+                    color='red'
+                    content='Like'
+                    icon='heart'
+                    label={{ basic: true, color: 'red', pointing: 'left', content: likes }}
                 />
+                <Link to="/">Go to dashboard</Link>
             </Segment>
 
         );
     }
 }
 
-export default stateLocal;
+const mapStateToProps = (state) => ({
+    likes: state.character.likes
+});
+
+export default connect(mapStateToProps, { increaseLikes })(stateLocal);
+
